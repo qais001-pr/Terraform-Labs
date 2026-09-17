569dda37a8664545782f612b8a6383ffc50b872d
ubuntu <Muhammad Qais>
2026-09-17 17:17:33 +0500

    add vpc module

---
+++ modules/vpc/main.tf
@@ -0,0 +1,72 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-vpc-lab"
+  }
+}
+
+resource "aws_subnet" "public-subnet-A" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-A"
+  }
+}
+
+
+
+resource "aws_subnet" "public-subnet-B" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-B"
+  }
+}
+
+
+
+
+resource "aws_subnet" "private-subnet-A" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-A"
+  }
+}
+
+
+resource "aws_subnet" "private-subnet-B" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-A"
+  }
+}
+
+
+resource "aws_internet_gateway" "internet_gateway_main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "terraform-internet-gateway"
+  }
+}
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "terraform-demo-public-route-table"
+  }
+}
---
+++ modules/vpc/output.tf
@@ -0,0 +1,6 @@
+output "region" {
+  value = var.region
+}
+output "public-subnet-1" {
+  value = aws_subnet.public-subnet-A
+}
