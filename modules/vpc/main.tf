61e9c704e7e831c18449d96522ced5d66a9b04f5
ubuntu <Muhammad Qais>
2026-09-17 17:31:24 +0500

    Add Private Route Table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -70,3 +70,11 @@ resource "aws_route_table" "public_route_table" {
     Name = "terraform-demo-public-route-table"
   }
 }
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "terraform-demo-private-route-table"
+  }
+}
+
